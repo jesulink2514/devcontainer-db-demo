781bba9bf4dcf267b83914a6318d909143c1c671
Jesus Angulo <jesus.angulo@outlook.com>
2022-07-26 19:36:45 -0500

    Initial commit

---
+++ .devcontainer/mssql/postCreateCommand.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+dacpac="false"
+sqlfiles="false"
+SApassword=$1
+dacpath=$2
+sqlpath=$3
+
+echo "SELECT * FROM SYS.DATABASES" | dd of=testsqlconnection.sql
+for i in {1..60};
+do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SApassword -d master -i testsqlconnection.sql > /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "SQL server ready"
+        break
+    else
+        echo "Not ready yet..."
+        sleep 1
+    fi
+done
+rm testsqlconnection.sql
+
+/opt/mssql-tools/bin/sqlcmd \
+   -S localhost -U sa -P $SApassword -d master \
+   -Q 'RESTORE DATABASE WideWorldImporters FROM DISK = "/bak/wwi.bak" WITH MOVE "WWI_Primary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", MOVE "WWI_UserData" TO "/var/opt/mssql/data/WideWorldImporters_userdata.ndf", MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "WWI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"'
